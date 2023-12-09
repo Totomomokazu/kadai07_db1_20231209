949010fafeb5a250e5a6c3148d49e3a584d6bb20
Totomomokazu <kanketomokazu@gmail.com>
2023-12-09 23:48:17 +0900

    登録処理ファイルの作成

--- index.php
+++ index.php
@@ -6,15 +6,14 @@
     <title>データ登録</title>
 </head>
 <body>
-    <form action="">
+    <form method="post" action="insert.php">
         <fieldset>
             <legend>本アンケート</legend>
-            <label>本の名前：<input type="text"></label><br>
-            <label>紹介用リンク：<input type="text"></label><br>
-            <label><textarea name="" id="" cols="30" rows="10"></textarea></label><br>
-            <input type="submit">
+            <label>本の名前：<input type="text" name="name"></label><br>
+            <label>紹介用リンク：<input type="text" name="email"></label><br>
+            <label><textarea name="content" id="" cols="30" rows="10"></textarea></label><br>
+            <input type="submit" value="送信">
         </fieldset>
-    </form>
-    
+    </form>    
 </body>
 </html>
