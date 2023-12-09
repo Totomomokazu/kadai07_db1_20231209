1810da4a28c5bf664e79365ab0151818bcb9b811
Totomomokazu <kanketomokazu@gmail.com>
2023-12-09 23:39:39 +0900

    indeファイルの作成

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>データ登録</title>
 </head>
 <body>
+    <form action="">
+        <fieldset>
+            <legend>本アンケート</legend>
+            <label>本の名前：<input type="text"></label><br>
+            <label>紹介用リンク：<input type="text"></label><br>
+            <label><textarea name="" id="" cols="30" rows="10"></textarea></label><br>
+            <input type="submit">
+        </fieldset>
+    </form>
     
 </body>
 </html>
